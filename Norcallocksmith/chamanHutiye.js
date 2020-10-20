1a897d208bb7bec99e80661cf3abdc40f9f81063
mozinomer <mozinomer12@gmail.com>
2020-10-20 14:31:34 +0500

    asd

--- Norcallocksmith/chamanHutiye.js
+++ Norcallocksmith/chamanHutiye.js
@@ -6,16 +6,72 @@ $('.sliderContainer').slick({
 $('.sliderCOntainerwhatwedo').slick({
 	slidesToShow: 3,
 	arrows: false,
-	dots: false
+	dots: false,
+	responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1.3,
+        slidesToScroll: 1,
+        centerMode: false,
+          infinite: false,
+
+      }
+    }
+  ]
 })
 $('.teamslider').slick({
 	slidesToShow: 4,
 	arrows: false,
-	dots: false
+	dots: false,
+	responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1.3,
+        slidesToScroll: 1,
+        centerMode: false,
+          infinite: false,
+
+      }
+    }
+  ]
 })
 $('.sliderCOntainertestimonials').slick({
 	slidesToShow: 4,
 	arrows: true,
 	slidesToScroll: 3,
-	dots: false
+	dots: false,
+	responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1.3,
+        slidesToScroll: 1,
+        centerMode: false,
+          infinite: false,
+
+      }
+    }
+  ]
 })
+
+if( $(window).width() < 767 ) {
+	$('div.menucontainer h6').click( function(e) {
+		$(this).parent().find('ul').slideToggle();
+		$(this).toggleClass('active');
+		$(this).parent().siblings().children().next().slideUp();
+		$(this).parent().siblings().find('h6').removeClass('active');
+		return false;
+	})
+}
+
+
+
+
+
+
+
+
+
+
