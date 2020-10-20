c3f46f0f2734dd6b75ea39565d321570fed0aacb
mozinomer <mozinomer12@gmail.com>
2020-10-20 10:47:54 +0500

    asd

--- Norcallocksmith/chamanHutiye.js
+++ Norcallocksmith/chamanHutiye.js
@@ -3,3 +3,18 @@ $('.sliderContainer').slick({
 	arrows: false,
 	dots: false
 })
+$('.sliderCOntainerwhatwedo').slick({
+	slidesToShow: 3,
+	arrows: false,
+	dots: false
+})
+$('.teamslider').slick({
+	slidesToShow: 4,
+	arrows: false,
+	dots: false
+})
+$('.sliderCOntainertestimonials').slick({
+	slidesToShow: 4,
+	arrows: true,
+	dots: false
+})
