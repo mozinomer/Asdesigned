640736c57d7e7d03fabef8f074114ed135c15d2d
mozinomer <mozinomer12@gmail.com>
2020-10-29 15:35:17 +0500

    asd

--- oilbabe/hy.js
+++ oilbabe/hy.js
@@ -2,17 +2,53 @@ $('.sliderproductSection').slick({
 	slidesToShow: 4,
 	slidesToScroll: 1,
 	dots: false,
-	arrows: false
+	arrows: false,
+	 responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows: false,
+      }
+    },
+  ]
 })
 $('.sliderSection3').slick({
 	slidesToShow: 3,
 	slidesToScroll: 1,
 	dots: false,
-	arrows: false
+	arrows: false,
+	 responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows: false,
+      }
+    },
+  ]
 })
 $('.slidetestimonials').slick({
 	slidesToShow: 3,
 	slidesToScroll: 1,
 	dots: false,
-	arrows: true
+	arrows: true,
+ 	responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows: false,
+      }
+    },
+  ]
 })
