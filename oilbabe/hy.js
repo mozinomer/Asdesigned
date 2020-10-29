037c8b858d627d7d1cade4bac02e964ee3e77664
mozinomer <mozinomer12@gmail.com>
2020-10-29 15:00:00 +0500

    DONE CSS

---
+++ oilbabe/hy.js
@@ -0,0 +1,18 @@
+$('.sliderproductSection').slick({
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	dots: false,
+	arrows: false
+})
+$('.sliderSection3').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	dots: false,
+	arrows: false
+})
+$('.slidetestimonials').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	dots: false,
+	arrows: true
+})
