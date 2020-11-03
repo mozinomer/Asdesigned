e0202cba6b1f20198d4d7b849907cc49bd8bb080
mozinomer <mozinomer12@gmail.com>
2020-11-03 11:19:29 +0500

    asd

--- oilbabe/hy.js
+++ oilbabe/hy.js
@@ -57,6 +57,38 @@ $('.hamburger').click( function(e) {
   $('.menuItems').slideToggle();
   $(this).toggleClass('active');
 });
+$(document).ready(function() {
+    $('.minus').click(function () {
+      var $input = $(this).parent().find('input');
+      var count = parseInt($input.val()) - 1;
+      count = count < 1 ? 1 : count;
+      $input.val(count);
+      $input.change();
+      return false;
+    });
+    $('.plus').click(function () {
+      var $input = $(this).parent().find('input');
+      $input.val(parseInt($input.val()) + 1);
+      $input.change();
+      return false;
+    });
+});
+$('.mainCarousal').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    fade: false,
+    asNavFor: '.forThumbnails'
+});
+$('.forThumbnails').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.mainCarousal',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true
+});
 
 
 
