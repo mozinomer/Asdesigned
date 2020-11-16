57bd21a2f1c899f584961d958a0bc27352e709b0
mozinomer <mozinomer12@gmail.com>
2020-11-16 20:34:55 +0500

    asd

--- pet/chutti_kr_mera_putt.js
+++ pet/chutti_kr_mera_putt.js
@@ -1,10 +1,34 @@
 $('.bannerHOmeContainer').slick({
 	slidesToShow: 1,
 	dots: true,
-	arrows: false
+	arrows: false,
+	 responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true,
+        arrows: false,
+      }
+    },
+  ]
 })
 $('.containerSLiderProduct').slick({
 	slidesToShow: 4,
 	dots: false,
-	arrows: true
+	arrows: true,
+	 responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true,
+        arrows: false,
+      }
+    },
+  ]
 })
--- pet/custom.js
+++ pet/custom.js
@@ -1,15 +1,40 @@
-$('.bannerHOmeContainer').slick({
-	slidesToShow: 1,
-	dots: false,
-	arrows: false
-})
-$('.containerSLiderProduct').slick({
-	slidesToShow: 4,
-	dots: false,
-	arrows: true
-})
+
 $('.sliderTestimonials').slick({
 	slidesToShow: 1,
 	dots: false,
 	arrows: true
+})
+$('.bannerHOmeContainer').slick({
+	slidesToShow: 1,
+	dots: true,
+	arrows: false,
+	 responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true,
+        arrows: false,
+      }
+    },
+  ]
+})
+$('.containerSLiderProduct').slick({
+	slidesToShow: 4,
+	dots: false,
+	arrows: true,
+	 responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true,
+        arrows: false,
+      }
+    },
+  ]
 })
