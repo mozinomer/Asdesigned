f80a65edfdc8188d68703782976eded13c5ed8fa
mozinomer <mozinomer12@gmail.com>
2020-11-16 19:53:42 +0500

    asd

---
+++ pet/Pawsitive Pet/custom.js
@@ -0,0 +1,15 @@
+$('.bannerHOmeContainer').slick({
+	slidesToShow: 1,
+	dots: false,
+	arrows: false
+})
+$('.containerSLiderProduct').slick({
+	slidesToShow: 4,
+	dots: false,
+	arrows: true
+})
+$('.sliderTestimonials').slick({
+	slidesToShow: 1,
+	dots: false,
+	arrows: true
+})
