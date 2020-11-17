c80bb2bd29d20819c8ddb5c6fbbd25bf6d2b882b
mozinomer <mozinomer12@gmail.com>
2020-11-17 16:03:36 +0500

    makign complet

--- staze/hor_sunao.js
+++ staze/hor_sunao.js
@@ -1,5 +1,21 @@
+$(document).ready(function() {
 $('.testimoina').slick({
 	slidesToShow: 1,
 	arrows: false,
 	dots: true
-})
+});
+$('.minus').click(function () {
+	var $input = $(this).parent().find('input');
+	var count = parseInt($input.val()) - 1;
+	count = count < 1 ? 1 : count;
+	$input.val(count);
+	$input.change();
+	return false;
+});
+$('.plus').click(function () {
+	var $input = $(this).parent().find('input');
+	$input.val(parseInt($input.val()) + 1);
+	$input.change();
+	return false;
+});
+});	
