ac3d7139eb209caddf824cbd1ac41a34130e5fa3
mozinomer <mozinomer12@gmail.com>
2020-11-19 11:17:21 +0500

    simplicity

--- simplicity/index.php
+++ simplicity/index.php
@@ -11,5 +11,89 @@
 </head>
 <body>
 
+
+	<div class="boxContainersHomepage">
+		<div class="container">
+			<h2 class="heading">
+				<strong>HEALTH BENEFITS</strong> OF DRINKING <strong>SIMPLICITY</strong> COLD PRESSED JUICE
+			</h2>
+			<ul class="containerBOxedHOmepage">
+				<li>
+					<div class="innerBoxContaienr">
+						<div class="numberIcon">
+							<span class="number">1</span>
+							<img src="./images/1.svg">
+						</div>
+						<div class="headingInner">
+							<h4>OPTIMIZE <br> NUTRIENT <br> INTAKE</h4>
+						</div>
+						<p class="boxContainerContent">Juicing helps you absorb all the nutrients from fruits and vegetables. This is so important because most of us have impaired digestion as a result of making less thanoptimal food choices over many years. This limits your body’s ability to absorb all the nutrients from the fruits and vegetables. Juicing will help to “pre-digest” them for you, so you will receive most of the nutrition allowing your body to heal, repair from what ailes it and be revitalized and funtion at it's highest level.</p>
+					</div>
+				</li>
+				<li>
+					<div class="innerBoxContaienr">
+						<div class="numberIcon">
+							<span class="number">1</span>
+							<img src="./images/1.svg">
+						</div>
+						<div class="headingInner">
+							<h4>Increased <br> Vitamins <br> &Minerals in Your <br> Diet</h4>
+						</div>
+						<p class="boxContainerContent">Juicing helps you absorb all the nutrients from fruits and vegetables. This is so important because most of us have impaired digestion as a result of making less thanoptimal food choices over many years. This limits your body’s ability to absorb all the nutrients from the fruits and vegetables. Juicing will help to “pre-digest” them for you, so you will receive most of the nutrition allowing your body to heal, repair from what ailes it and be revitalized and funtion at it's highest level.</p>
+					</div>
+				</li>
+				<li>
+					<div class="innerBoxContaienr">
+						<div class="numberIcon">
+							<span class="number">1</span>
+							<img src="./images/1.svg">
+						</div>
+						<div class="headingInner">
+							<h4>OPTIMIZE <br> NUTRIENT <br> INTAKE</h4>
+						</div>
+						<p class="boxContainerContent">SIMPLICITY Cold Pressed Juice can be likened to a “living broth,” as it is teeming with micronutrients that many people are lacking. Vitamins and minerals are two of the main types of nutrients that your body needs to survive and stay healthy.</p>
+						<p class="boxContainerContent">function the way it should. Vitamins have different jobs—helping you resist infections, keeping your nerves healthy, and helping your body get energy from food/juice, and your blood to clot properly.</p>
+					</div>
+				</li>
+				<li>
+					<div class="innerBoxContaienr">
+						<div class="numberIcon">
+							<span class="number">1</span>
+							<img src="./images/1.svg">
+						</div>
+						<div class="headingInner">
+							<h4>OPTIMIZE <br> NUTRIENT <br> INTAKE</h4>
+						</div>
+						<p class="boxContainerContent">Juicing helps you absorb all the nutrients from fruits and vegetables. This is so important because most of us have impaired digestion as a result of making less thanoptimal food choices over many years. This limits your body’s ability to absorb all the nutrients from the fruits and vegetables. Juicing will help to “pre-digest” them for you, so you will receive most of the nutrition allowing your body to heal, repair from what ailes it and be revitalized and funtion at it's highest level.</p>
+					</div>
+				</li>
+				<li>
+					<div class="innerBoxContaienr">
+						<div class="numberIcon">
+							<span class="number">1</span>
+							<img src="./images/1.svg">
+						</div>
+						<div class="headingInner">
+							<h4>OPTIMIZE <br> NUTRIENT <br> INTAKE</h4>
+						</div>
+						<p class="boxContainerContent">Juicing helps you absorb all the nutrients from fruits and vegetables. This is so important because most of us have impaired digestion as a result of making less thanoptimal food choices over many years. This limits your body’s ability to absorb all the nutrients from the fruits and vegetables. Juicing will help to “pre-digest” them for you, so you will receive most of the nutrition allowing your body to heal, repair from what ailes it and be revitalized and funtion at it's highest level.</p>
+					</div>
+				</li>
+				<li>
+					<div class="innerBoxContaienr">
+						<div class="numberIcon">
+							<span class="number">1</span>
+							<img src="./images/1.svg">
+						</div>
+						<div class="headingInner">
+							<h4>OPTIMIZE <br> NUTRIENT <br> INTAKE</h4>
+						</div>
+						<p class="boxContainerContent">Juicing helps you absorb all the nutrients from fruits and vegetables. This is so important because most of us have impaired digestion as a result of making less thanoptimal food choices over many years. This limits your body’s ability to absorb all the nutrients from the fruits and vegetables. Juicing will help to “pre-digest” them for you, so you will receive most of the nutrition allowing your body to heal, repair from what ailes it and be revitalized and funtion at it's highest level.</p>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+
 </body>
 </html>
