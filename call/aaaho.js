1961054d53fe73bece49db10739b43e7af35f201
mozinomer <mozinomer12@gmail.com>
2020-11-25 11:38:43 +0500

    asd

--- call/aaaho.js
+++ call/aaaho.js
@@ -6,3 +6,19 @@ $('.sliderTestimonials').slick({
 	slidesToShow: 1,
 	slidesToScroll: 1,
 });
+
+$('.sliderFor').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: false,
+	fade: true,
+	asNavFor: '.sliderContentContainerQuantum'
+});
+$('.sliderContentContainerQuantum').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	asNavFor: '.sliderFor',
+	dots: true,
+	centerMode: true,
+	focusOnSelect: true
+});
