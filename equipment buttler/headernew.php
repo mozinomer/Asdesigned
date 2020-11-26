46df88ea3197b4cd742cbf807b2e11b909a17cc6
mozinomer <mozinomer12@gmail.com>
2020-11-26 23:07:04 +0500

    asd

--- equipment buttler/headernew.php
+++ equipment buttler/headernew.php
@@ -21,9 +21,37 @@
 		<div class="closerMenu"></div>
 		<div class="menuNameContainer">
 			<h2 class="headingN">Menu</h2>
-			<ul class="menuMainNew">
-				<li><a href="#" class="menuName"></a></li>
-			</ul>
+			<div class="menuContainerNew">
+				<?php if( has_nav_menu( 'primary' ) )
+		            wp_nav_menu( array(
+			                'container' => false,
+							'theme_location' => 'primary',
+							'container_class'
+	                    )
+				); ?>
+			</div>
+		</div>
+		<div class="bottomContainermenuNew">
+			<div class="socialLinkContainer">
+				<ul class="socialLInksNws">
+					<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+					<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				</ul>
+				<ul class="contactInfo">
+					<li><?php if(get_field('header_phone','option')): ?>
+                        	<a href="tel:<?php the_field('header_phone','option'); ?>"><i class="fas fa-phone-alt"></i><?php the_field('header_phone','option'); ?></a>
+                        <?php endif; ?></li>
+                        <li>
+                        	<?php if(get_field('header_fax','option')): ?>	
+                        	<a href="tel:<?php the_field('header_fax','option'); ?>"><i class=""></i><?php the_field('header_fax','option'); ?></a> <?php endif; ?>
+                        </li>
+                        <li>
+                        	?php if(get_field('header_email','option')): ?>
+                        	<a href="mailto:<?php the_field('header_email','option'); ?>"><i class="fas fa-envelope"></i><?php the_field('header_email','option'); ?></a>
+        				<?php endif; ?>
+                        </li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </div>
