04544625f49a1672ce2fdc7408828b3569187359
mozinomer <mozinomer12@gmail.com>
2021-01-01 16:01:43 +0500

    completed the css for the hoepage for desktop and for responsive too

--- npw/eww.js
+++ npw/eww.js
@@ -2,4 +2,18 @@ $('.sliderProductgallery').slick({
 	slidesToShow: 3,
 	arrows: false,
 	dots: false,
+	responsive: [
+	{
+		breakpoint: 767,
+		settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			infinite: true,
+			dots: true
+		}
+	},
+    ]
+})
+$('.menuMobileContainer').click( function(e) {
+	$('.mainMenu').slideToggle();
 })
