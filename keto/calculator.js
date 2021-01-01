d2d2b480bb2ee00b0a10e509104c1d12eb53e32d
mozinomer <mozinomer12@gmail.com>
2021-01-01 17:44:06 +0500

    asd

--- keto/calculator.js
+++ keto/calculator.js
@@ -5,11 +5,18 @@ var yourweight = 0;
 var yourheight = 0;
 var fatPercentage = 0;
 var activityLevelCheck = 0;
+var Male = true;
+var Unit = true;
 
 
 /* variables For the Values */
 $('#genderCheck input').on('change', function(e) {
 	// console.log($(this).val())
+	if ($(this).attr('id') === "Male") {
+		return Male = true;
+	} else {
+		return Male = false;
+	}
 })
 $('#yourweightValue input').on('change', function(e) {
 	return yourweight = $(this).val() * 10;
@@ -35,16 +42,21 @@ $('#meterUnit input').on('change', function(e) {
 	var unitType = $(this).val();
 	if (unitType === 'metricSelected') {
 		$('#yourweight').attr("placeholder", "write weight in Kg");
+		return Unit = false;
 	} else {
 		$('#yourweight').attr("placeholder", "write weight in lbs");
+		return Unit = true;
 	}
 });
 $('#calculateresult').click( function(e) {
+	console.clear();
 	var leanMass = yourweight * (1 - (fatPercentage));
-	console.log('yourage' + yourage);
-	console.log('yourweight' + yourweight);
-	console.log('yourheight' + yourheight);
-	console.log('fatPercentage' + fatPercentage);
-	console.log('activityLevelCheck' + activityLevelCheck);
-	console.log('leanMass' + leanMass);
+	console.log('is Male = ' + Male);
+	console.log('is Unit Lbs = ' + Male);
+	console.log('yourage = ' + yourage);
+	console.log('yourweight = ' + yourweight);
+	console.log('yourheight = ' + yourheight);
+	console.log('fatPercentage = ' + fatPercentage);
+	console.log('activityLevelCheck = ' + activityLevelCheck);
+	console.log('leanMass = ' + leanMass);
 })
