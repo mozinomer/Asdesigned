b91ea89fa96198d32ae7ca8f855f4c062f241e43
mozinomer <mozinomer12@gmail.com>
2021-01-04 11:02:43 +0500

    made the shop pae

--- npw/eww.js
+++ npw/eww.js
@@ -13,7 +13,18 @@ $('.sliderProductgallery').slick({
 		}
 	},
     ]
-})
+});
 $('.menuMobileContainer').click( function(e) {
 	$('.mainMenu').slideToggle();
+});
+$(document).ready(function(){
+  $(".containerProductShopPage").slice(0, 3).show();
+  $("#loadMore").on("click", function(e){
+    e.preventDefault();
+    $(".containerProductShopPage:hidden").slice(0, 3).slideDown();
+    if($(".containerProductShopPage:hidden").length == 0) {
+      $("#loadMore").text("No More Product Found").addClass("noContent");
+    }
+  });
+  
 })
