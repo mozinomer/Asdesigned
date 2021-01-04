207958131590bc0a73934b36861cbd06473b4488
mozinomer <mozinomer12@gmail.com>
2021-01-04 12:49:10 +0500

    about page without the bootstrap

--- npw/eww.js
+++ npw/eww.js
@@ -12,19 +12,27 @@ $('.sliderProductgallery').slick({
 			dots: true
 		}
 	},
-    ]
+	]
 });
 $('.menuMobileContainer').click( function(e) {
 	$('.mainMenu').slideToggle();
 });
 $(document).ready(function(){
-  $(".containerProductShopPage").slice(0, 3).show();
-  $("#loadMore").on("click", function(e){
-    e.preventDefault();
-    $(".containerProductShopPage:hidden").slice(0, 3).slideDown();
-    if($(".containerProductShopPage:hidden").length == 0) {
-      $("#loadMore").text("No More Product Found").addClass("noContent");
-    }
-  });
-  
+	$(".containerProductShopPage").slice(0, 3).show();
+	$("#loadMore").on("click", function(e){
+		e.preventDefault();
+		$(".containerProductShopPage:hidden").slice(0, 3).slideDown();
+		if($(".containerProductShopPage:hidden").length == 0) {
+			$("#loadMore").text("No More Product Found").addClass("noContent");
+		}
+	});
+
 })
+
+if ($(window).width() < 767) {
+	$('.listSections h6').click( function(e) {
+		$(this).parent().find('ul').slideToggle();
+		$(this).parent().siblings().children().next().slideUp();
+		return false;
+	});
+}
