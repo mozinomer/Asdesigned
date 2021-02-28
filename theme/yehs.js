f1c0c086fe46d815883b5c72fa422013b5c679bf
mozinomer <mozinomer12@gmail.com>
2021-02-28 17:37:28 +0500

    ad

---
+++ theme/yehs.js
@@ -0,0 +1,20 @@
+$('.sliderCOntainerBanner').slick({
+	slidesToShow: 1,
+	dots: true,
+	arrows: false,
+})
+$('.sliderCOntainerProductCOntainer').slick({
+	slidesToShow: 6,
+	dots: false,
+	arrows: true,
+})
+$('.productSlider .sliderCOntainerProduct').slick({
+	slidesToShow: 6,
+	dots: false,
+	arrows: true,
+})
+$('.newCollection .sliderCOntainerProduct').slick({
+	slidesToShow: 4,
+	dots: false,
+	arrows: true,
+})
